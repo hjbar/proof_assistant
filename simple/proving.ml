50eb3ab238c79687fa974d48415fc60cac3bbf70
hjbar <hjbar@protonmail.com>
2024-11-08 22:17:23 +0100

    Store commands in historical in interactive mode

--- simple/proving.ml
+++ simple/proving.ml
@@ -5,14 +5,14 @@ open Expr
 open Prover
 
 (** The prove function for the interactive prover *)
-let rec prove env a =
+let rec prove env a out_c =
   print_endline @@ string_of_seq (env, a);
   print_string "? ";
   flush_all ();
 
   let error e =
     print_endline e;
-    prove env a
+    prove env a out_c
   in
 
   let cmd, arg =
@@ -29,15 +29,20 @@ let rec prove env a =
     (c, a)
   in
 
+  let write_cmd () = output_string out_c @@ Format.sprintf "%s %s\n" cmd arg in
+
   match cmd with
   | "intro" -> begin
     match a with
     | Imp (a, b) ->
       if arg = "" then error "Please provide an argument for intro."
-      else
+      else begin
+        write_cmd ();
+
         let x = arg in
-        let t = prove ((x, a) :: env) b in
+        let t = prove ((x, a) :: env) b out_c in
         Abs (x, a, t)
+      end
     | _ -> error "Don't know how to introduce this."
   end
   | "elim" -> begin
@@ -46,7 +51,9 @@ let rec prove env a =
     else
       match Option.get ty_opt with
       | Imp (a, _b) ->
-        let t = prove env a in
+        write_cmd ();
+
+        let t = prove env a out_c in
         App (Var arg, t)
       | _ -> error "Don't know how to eliminate this."
   end
@@ -55,7 +62,10 @@ let rec prove env a =
     if Option.is_none @@ List.assoc_opt arg env then
       error "This variable is not in the context."
     else if infer_type env t <> a then error "Not the right type."
-    else t
+    else begin
+      write_cmd ();
+      t
+    end
   | cmd -> error @@ "Unknown command: " ^ cmd
 
 (** Main function of the interactive prover *)
@@ -64,9 +74,17 @@ let main () =
   let a = input_line stdin in
   let a = ty_of_string a in
 
+  let () =
+    try Sys.mkdir "tmp" 0o775 with
+    | _ -> ()
+  in
+  let out_c = open_out_gen [ Open_creat; Open_wronly ] 0o664 "tmp/tmp.proof" in
+
   print_endline "Let's prove it.";
-  let t = prove [] a in
+  let t = prove [] a out_c in
   print_endline "done.";
+
+  close_out out_c;
 
   print_endline "Proof term is";
   print_endline (string_of_tm t);
