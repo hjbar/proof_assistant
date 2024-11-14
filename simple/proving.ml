8d1035721324bb171e909b939298da92a5a77241
hjbar <hjbar@protonmail.com>
2024-11-14 19:14:54 +0100

    Work in progress (until 2.10 with some todo)

--- simple/proving.ml
+++ simple/proving.ml
@@ -43,6 +43,20 @@ let rec prove env a out_c =
         let t = prove ((x, a) :: env) b out_c in
         Abs (x, a, t)
       end
+    | And (a, b) ->
+      if arg <> "" then
+        error "Please provide no argument for intro conjonction."
+      else begin
+        write_cmd ();
+
+        let t1 = prove env a out_c in
+        let t2 = prove env b out_c in
+        Pair (t1, t2)
+      end
+    | True ->
+      write_cmd ();
+
+      Unit
     | _ -> error "Don't know how to introduce this."
   end
   | "elim" -> begin
@@ -55,7 +69,42 @@ let rec prove env a out_c =
 
         let t = prove env a out_c in
         App (Var arg, t)
+      | Or (l, r) ->
+        write_cmd ();
+
+        let t1 = prove env (Imp (l, a)) out_c in
+        let t2 = prove env (Imp (r, a)) out_c in
+        Case (Var arg, t1, t2)
       | _ -> error "Don't know how to eliminate this."
+  end
+  | "cut" -> begin
+    match a with
+    | Imp _ ->
+      write_cmd ();
+
+      let x = ty_of_string arg in
+      let t2 = prove env (Imp (x, a)) out_c in
+      let t1 = prove env x out_c in
+      App (t1, t2)
+    | _ -> error "Don't know how to cut this"
+  end
+  | "left" -> begin
+    match a with
+    | Or (a, b) ->
+      write_cmd ();
+
+      let t = prove env a out_c in
+      Left (t, b)
+    | _ -> error "Don't know how to left this"
+  end
+  | "right" -> begin
+    match a with
+    | Or (a, b) ->
+      write_cmd ();
+
+      let t = prove env b out_c in
+      Right (a, t)
+    | _ -> error "Don't know how to left this"
   end
   | "exact" ->
     let t = tm_of_string arg in
@@ -66,6 +115,34 @@ let rec prove env a out_c =
       write_cmd ();
       t
     end
+  | "fst" -> begin
+    let ty_opt = List.assoc_opt arg env in
+    if Option.is_none ty_opt then error "This variable is not in the context."
+    else
+      match Option.get ty_opt with
+      | And (a, _b) ->
+        let t = Fst (tm_of_string arg) in
+        if infer_type env t <> a then error "Not the right type"
+        else begin
+          write_cmd ();
+          t
+        end
+      | _ -> error "Don't know how to snd this."
+  end
+  | "snd" -> begin
+    let ty_opt = List.assoc_opt arg env in
+    if Option.is_none ty_opt then error "This variable is not in the context."
+    else
+      match Option.get ty_opt with
+      | And (_a, b) ->
+        let t = Snd (tm_of_string arg) in
+        if infer_type env t <> b then error "Not the right type"
+        else begin
+          write_cmd ();
+          t
+        end
+      | _ -> error "Don't know how to snd this."
+  end
   | cmd -> error @@ "Unknown command: " ^ cmd
 
 (** Main function of the interactive prover *)
