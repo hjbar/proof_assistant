3ff28c9f9f65a15b3e9c10ffca33d291b17185a7
hjbar <hjbar@protonmail.com>
2024-11-27 22:54:30 +0100

    Jusqu'à 5.8 Conversion inclu

--- dependent/prover.ml
+++ dependent/prover.ml
@@ -17,13 +17,7 @@ let rec to_string = function
   | Pi (x, t1, t2) ->
     let s1, s2 = (to_string t1, to_string t2) in
     Format.sprintf "(Π (%s : %s) → %s)" x s1 s2
-  | Nat -> failwith "todo"
-  | Z -> failwith "todo"
-  | S _t -> failwith "todo"
-  | Ind (_t1, _t2, _t3, _t4) -> failwith "todo"
-  | Eq (_t1, _t2) -> failwith "todo"
-  | Refl _t -> failwith "todo"
-  | J (_t1, _t2, _t3, _t4, _t5) -> failwith "todo"
+  | _ -> failwith "to_string todo"
 
 (** Returns a fresh variable name at each call *)
 let fresh_var =
@@ -37,16 +31,21 @@ let rec subst x t = function
   | Type -> Type
   | Var v when v = x -> t
   | Var _ as u -> u
-  | App (u1, u2) -> App (subst x t u1, subst x t u2)
+  | App (u1, u2) ->
+    let u1' = subst x t u1 in
+    let u2' = subst x t u2 in
+    App (u1', u2')
   | Abs (v, ty, u) ->
     let x' = fresh_var () in
+    let ty' = subst v (Var x') ty in
     let u' = subst v (Var x') u in
-    Abs (x', ty, subst x t u')
+    Abs (x', subst x t ty', subst x t u')
   | Pi (v, ty, u) ->
     let x' = fresh_var () in
+    let ty' = subst v (Var x') ty in
     let u' = subst v (Var x') u in
-    Pi (x', ty, subst x t u')
-  | _ -> assert false
+    Pi (x', subst x t ty', subst x t u')
+  | _ -> failwith "subst todo"
 
 (** Provide the string representation of a context *)
 let string_of_context (ctx : context) =
@@ -57,3 +56,66 @@ let string_of_context (ctx : context) =
     | Some value -> Format.sprintf "%s : %s = %s" x s (to_string value)
   in
   ctx |> List.rev |> List.map content_to_string |> String.concat ", "
+
+(** Computes the normal form of an expression *)
+let ctx_lookup (ctx : context) x =
+  match List.assoc_opt x ctx with
+  | None -> None
+  | Some (_, value) -> value
+
+let rec normalize (ctx : context) = function
+  | Type -> Type
+  | Var x as t -> begin
+    match ctx_lookup ctx x with
+    | None -> t
+    | Some value -> normalize ctx value
+  end
+  | App (t1, t2) -> begin
+    let t1' = normalize ctx t1 in
+    let t2' = normalize ctx t2 in
+    match t1' with
+    | Abs (x, _, tm) -> normalize ctx (subst x t2' tm)
+    | _ -> App (t1', t2')
+  end
+  | Abs (x, ty, tm) ->
+    let ty' = normalize ctx ty in
+    let tm' = normalize ctx tm in
+    Abs (x, ty', tm')
+  | Pi (x, ty, tm) ->
+    let ty' = normalize ctx ty in
+    let tm' = normalize ctx tm in
+    Pi (x, ty', tm')
+  | _ -> failwith "normalize todo"
+
+(** Tests whether two terms are α-convertible *)
+let rec alpha t1 t2 =
+  match (t1, t2) with
+  | Type, Type -> true
+  | Var x, Var x' -> x = x'
+  | App (t, u), App (t', u') -> alpha t t' && alpha u u'
+  | Abs (x1, ty1, tm1), Abs (x2, ty2, tm2)
+  | Pi (x1, ty1, tm1), Pi (x2, ty2, tm2) ->
+    let x = Var (fresh_var ()) in
+
+    let ty1' = subst x1 x ty1 in
+    let tm1' = subst x1 x tm1 in
+
+    let ty2' = subst x2 x ty2 in
+    let tm2' = subst x2 x tm2 in
+
+    alpha ty1' ty2' && alpha tm1' tm2'
+  | Nat, Nat
+  | Z, Z
+  | S _, S _
+  | Ind _, Ind _
+  | Eq _, Eq _
+  | Refl _, Refl _
+  | J _, J _ ->
+    failwith "alpha todo"
+  | _ -> false
+
+(** Determines whether two terms are αβ-convertible *)
+let conv ctx t1 t2 =
+  let t1' = normalize ctx t1 in
+  let t2' = normalize ctx t2 in
+  alpha t1' t2'
--- dependent/test.ml
+++ dependent/test.ml
@@ -27,22 +27,49 @@ let test_to_string () =
   assert (res = obj)
 
 let test_subst () =
+  let res = subst "x" (Var "y") Type in
+  let obj = Type in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") (Var "x") in
+  let obj = Var "y" in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") (Var "z") in
+  let obj = Var "z" in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") (App (Var "x", Var "z")) in
+  let obj = App (Var "y", Var "z") in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") @@ Abs ("z", Var "x", App (Var "z", Var "x")) in
+  let obj = Abs ("x", Var "y", App (Var "x", Var "y")) in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") @@ Abs ("x", Var "x", App (Var "x", Var "z")) in
+  let obj = Abs ("x", Var "x", App (Var "x", Var "z")) in
+  assert (alpha res obj);
+
+  let res = subst "x" (Var "y") @@ Pi ("x", Var "x", App (Var "x", Var "z")) in
+  let obj = Pi ("x", Var "x", App (Var "x", Var "z")) in
+  assert (alpha res obj);
+
   let res =
     subst "x"
       (Abs ("x", Var "A", Var "x"))
       (App (Var "x", Abs ("y", Var "B", Var "x")))
   in
-
   let obj =
     App
       ( Abs ("x", Var "A", Var "x")
-      , Abs ("x1", Var "B", Abs ("x", Var "A", Var "x")) )
+      , Abs ("y", Var "B", Abs ("x", Var "A", Var "x")) )
   in
-  assert (res = obj);
+  assert (alpha res obj);
 
   let res = subst "x" (Var "y") (Abs ("y", Var "B", Var "x")) in
-  let not_obj = Abs ("x1", Var "B", Var "x1") in
-  assert (res <> not_obj)
+  let obj = Abs ("x", Var "B", Var "y") in
+  assert (alpha res obj)
 
 let test_string_of_context () =
   let res = string_of_context [] in
@@ -64,6 +91,242 @@ let test_string_of_context () =
   let obj = "x : A = x, y : Set" in
   assert (res = obj)
 
+let test_normalize () =
+  let res = normalize [] Type in
+  let obj = Type in
+  assert (res = obj);
+
+  let res = normalize [] @@ Var "x" in
+  let obj = Var "x" in
+  assert (res = obj);
+
+  let res = normalize [ ("x", (Type, Some (Var "y"))) ] @@ Var "x" in
+  let obj = Var "y" in
+  assert (res = obj);
+
+  let res = normalize [] @@ App (Var "f", Var "x") in
+  let obj = App (Var "f", Var "x") in
+  assert (res = obj);
+
+  let res =
+    normalize
+      [ ("id", (Pi ("x", Type, Var "x"), Some (Abs ("x", Type, Var "x")))) ]
+    @@ App (Var "id", Var "z")
+  in
+  let obj = Var "z" in
+  assert (res = obj);
+
+  let res = normalize [] @@ Abs ("x", Type, Var "x") in
+  let obj = Abs ("x", Type, Var "x") in
+  assert (res = obj);
+
+  let res = normalize [] @@ Pi ("x", Type, Var "x") in
+  let obj = Pi ("x", Type, Var "x") in
+  assert (res = obj);
+
+  let res =
+    normalize [ ("A", (Type, Some (Var "B"))) ] @@ Pi ("x", Var "A", Var "x")
+  in
+  let obj = Pi ("x", Var "B", Var "x") in
+  assert (res = obj);
+
+  let res =
+    normalize [] @@ App (Abs ("x", Var "A", App (Var "x", Var "x")), Type)
+  in
+  let obj = App (Type, Type) in
+  assert (res = obj);
+
+  let res =
+    normalize []
+    @@ App
+         ( Abs ("x", Var "A", App (Abs ("y", Var "B", Var "y"), Var "x"))
+         , Var "z" )
+  in
+  let obj = Var "z" in
+  assert (res = obj)
+
+let test_alpha () =
+  let res = alpha Type Type in
+  let obj = true in
+  assert (res = obj);
+
+  let res = alpha (Var "x") (Var "x") in
+  let obj = true in
+  assert (res = obj);
+
+  let res = alpha (Var "x") (Var "y") in
+  let obj = false in
+  assert (res = obj);
+
+  let res = alpha (App (Var "f", Var "x")) (App (Var "f", Var "x")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = alpha (App (Var "f", Var "x")) (App (Var "f", Var "y")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res = alpha (Abs ("x", Type, Var "x")) (Abs ("y", Type, Var "y")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = alpha (Abs ("x", Type, Var "x")) (Abs ("x", Var "A", Var "x")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    alpha (Abs ("x", Type, Var "x")) (Abs ("x", Type, App (Var "x", Var "y")))
+  in
+  let obj = false in
+  assert (res = obj);
+
+  let res = alpha (Pi ("x", Type, Var "x")) (Pi ("y", Type, Var "y")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = alpha (Pi ("x", Type, Var "x")) (Pi ("x", Var "A", Var "x")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    alpha (Pi ("x", Type, Var "x")) (Pi ("x", Type, App (Var "x", Var "y")))
+  in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    alpha
+      (App (Abs ("x", Type, Var "x"), Var "y"))
+      (App (Abs ("z", Type, Var "z"), Var "y"))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    alpha
+      (Abs ("x", Pi ("y", Type, Var "y"), Var "x"))
+      (Abs ("w", Pi ("w", Type, Var "w"), Var "w"))
+  in
+  let obj = true in
+  assert (res = obj)
+
+let test_conv () =
+  let res = conv [] Type Type in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (Var "x") (Var "x") in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (Var "x") (Var "y") in
+  let obj = false in
+  assert (res = obj);
+
+  let res = conv [] (App (Var "f", Var "x")) (App (Var "f", Var "x")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (App (Var "f", Var "x")) (App (Var "f", Var "y")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res = conv [] (Abs ("x", Type, Var "x")) (Abs ("y", Type, Var "y")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (Abs ("x", Type, Var "x")) (Abs ("x", Var "A", Var "x")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    conv [] (Abs ("x", Type, Var "x")) (Abs ("x", Type, App (Var "x", Var "y")))
+  in
+  let obj = false in
+  assert (res = obj);
+
+  let res = conv [] (Pi ("x", Type, Var "x")) (Pi ("y", Type, Var "y")) in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (Pi ("x", Type, Var "x")) (Pi ("x", Var "A", Var "x")) in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    conv [] (Pi ("x", Type, Var "x")) (Pi ("x", Type, App (Var "x", Var "y")))
+  in
+  let obj = false in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (App (Abs ("x", Type, Var "x"), Var "y"))
+      (App (Abs ("z", Type, Var "z"), Var "y"))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (Abs ("x", Pi ("y", Type, Var "y"), Var "x"))
+      (Abs ("z", Pi ("w", Type, Var "w"), Var "z"))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (App (Abs ("x", Type, Var "x"), Type)) Type in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (App (Abs ("x", Pi ("y", Type, Var "y"), Var "x"), Type))
+      (App (Abs ("z", Pi ("w", Type, Var "w"), Var "z"), Type))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res = conv [] (App (Abs ("x", Type, Var "x"), Type)) Type in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv [] (App (Abs ("x", Type, App (Var "x", Type)), Type)) (App (Type, Type))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv [] (App (Abs ("x", Pi ("y", Type, Var "y"), Var "x"), Type)) Type
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (App (Abs ("x", Type, Abs ("y", Type, Var "x")), Type))
+      (Abs ("y", Type, Type))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (App (Abs ("x", Type, App (Abs ("y", Type, Var "y"), Var "x")), Type))
+      (App (Abs ("y", Type, Var "y"), Type))
+  in
+  let obj = true in
+  assert (res = obj);
+
+  let res =
+    conv []
+      (App (Abs ("x", Pi ("y", Type, Var "y"), Abs ("z", Type, Var "z")), Type))
+      (Abs ("z", Type, Var "z"))
+  in
+  let obj = true in
+  assert (res = obj)
+
 (** General testing function *)
 
 let test_all_functions () =
@@ -72,5 +335,8 @@ let test_all_functions () =
   test_to_string ();
   test_subst ();
   test_string_of_context ();
+  test_normalize ();
+  test_alpha ();
+  test_conv ();
 
   Format.printf "\nDependent prover : OK\n\n%!"
